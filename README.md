a2aca7cf796c6de51356a48c707f5a601d0c6985
Enrique López Mañas <eenriquelopez@gmail.com>
2015-04-07 22:12:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+espresso-android-set-up
+==========================
+
+UI Tests with Espresso — Andorid Studio set up for beginners
+
+You have your Android project. You use Android Studio. You want to easly write tests for your UI. 
+
+Read this post for detailed instruction https://medium.com/p/c476d3b5ba45/
+
+UPDATE: The original post is out of date, the commands have been updated
+
+```
+gradle clean
+gradle connectedCheck build
+
+```
+
+
---
+++ src/androidTest/java/com/dropsport/espressoreadyproject/tests/MainActivityInstrumentationTest.java
@@ -0,0 +1,43 @@
+package com.dropsport.espressoreadyproject.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.dropsport.espressoreadyproject.MainActivity;
+import com.dropsport.espressoreadyproject.R;
+
+
+import static android.test.ViewAsserts.assertOnScreen;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private MainActivity mMainActivity;
+    private TextView mHelloWorldTextView;
+
+    @SuppressWarnings( "deprecation" )
+    public MainActivityInstrumentationTest() {
+        super("com.dropsport.espressoreadyproject", MainActivity.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        mMainActivity = getActivity();
+        mHelloWorldTextView = (TextView) mMainActivity.findViewById(R.id.hello_world_text);
+    }
+
+    public void testTextView() {
+        assertOnScreen(mMainActivity.getWindow().getDecorView(), mHelloWorldTextView);
+    }
+
+    public void testLabel() {
+        onView(withId(R.id.hello_world_text)).check(matches(withText("Hello world!")));
+    }
+
+    //public void testFalseLabel() {
+    //    onView(withId(R.id.hello_world_text)).check(matches(withText("What a label!")));
+    //}
+}
